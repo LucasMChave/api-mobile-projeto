0748b6efbf9e910b1e8f5ad3015bb3b63f6c5ffc
LucasMouratoCH <132353766+LucasMChave@users.noreply.github.com>
2025-11-25 10:12:38 -0300

    coloração e design

--- ex02-expo/app/index.tsx
+++ ex02-expo/app/index.tsx
@@ -13,30 +13,30 @@ export default function Home() {
         <ScrollView>
             <View style = {styles.Home}>
                 <Text style = {styles.title}>Lucas Mourato Aureliano de Melo</Text>
-                <Text>RA: 00000848725 | Estudante de C3 Unicap de 5º periodo | 22 anos de Idade</Text>
+                <Text style = {styles.description}>Estudante de C3 Unicap de 5º periodo{"\n"}22 anos de Idade{"\n"}RA: 00000848725</Text>
                 <Image source={require('../assets/pics/foto.jpg')} style = {styles.picture}/>
                 <Text style = {styles.quote}>"Capre Diem" - Horácio{""}
                     <Text style = {styles.sup}>23a.C</Text>
                 </Text>
                 
-                <Text style={styles.segment}>• Biografia</Text>
-                <Text>-{"\t"}Durante minha infância e adolescência, sempre gostei de videogames, passava minhas tardes jogando e cresci com muitas das 
+                <Text style={styles.segment}>• Biografia •</Text>
+                <Text style={styles.bio}>-Durante minha infância e adolescência, sempre gostei de videogames, passava minhas tardes jogando e cresci com muitas das 
                 minhas inspirações artisticas baseadas nisso, simliar a pessoas que cresceram vendo flimes ou lendo livros.
                 Mesmo assim, minha vida educacional nunca foi gravemente afetada, mesmo com dificuldades em certos periodos de tempo
                 sempre mantive minhas notas iguais ou acima da média com eficiencia em Matemática e Inglês.  
                 Ao me ingressar na faculdade, muitas das minhas dificuldades se ampliaram, graças a pandemia que ocorreu durante o 3º ano
                 do meu ensino médio e minha dificuldade em manter os estudos em dia durante essa época me afetando durante meus anos seguintes,
                 mesmo com esses impenclihos e cadeiras problemas que tive durante o curso, ainda sigo em frente com intenção de completar
-                o curso de C3, para me provar capaz e ingressar na vida profissional.{"\n"}</Text>
+                o curso de C3, para me provar capaz e ingressar na vida profissional.</Text>
 
 
-                <Text style={styles.segment}>• Capacidades</Text>
+                <Text style={styles.segment}>• Capacidades •</Text>
                 <Text style={styles.topics}>- Programação em C e Java;{"\n"}
                 - Conhecimento básico de HTML-CSS;{"\n"}
                 - Conhecimento avançado da língua inglesa;{"\n"}
                 - Manejamento de projetos em grupo;{"\n"}</Text>
 
-                <Text style={styles.segment}>• Referencias</Text>
+                <Text style={styles.segment}>• Referencias •</Text>
                 <Link href="https://www.linkedin.com/in/lucas-mourato-5b6a062b5/" style={styles.hyperlink}>Linkedin{""}
                     <Image source={require('../assets/other/linkedin_icon.png')} style={styles.logo}/>
                 </Link>
@@ -49,6 +49,7 @@ export default function Home() {
                 <Link href="https://www.youtube.com/@lukey3474" style={styles.hyperlink}>Youtube{""}
                     <Image source={require('../assets/other/Youtube_logo.png')} style={styles.logo}/>
                 </Link>
+                
                 
                 <Button title = "Jogo da Velha" onPress = {handleMessage}/>
             </View>
--- ex02-expo/app/style.ts
+++ ex02-expo/app/style.ts
@@ -8,31 +8,60 @@ export const styles = StyleSheet.create({
         justifyContent: 'center',
         padding: 16,
         gap: 12,
-        marginTop: 22,
+        backgroundColor: "#fcbacb",
     },
     
     title: {
-        color: "purple",
+        width: "90%",
+        color: "#750180",
         fontSize: 34,
         fontWeight: "bold",
         fontStyle: "italic",
+        marginTop: 22,
+        backgroundColor: "#FC94AF",
+        textAlign: "center",
+    },
+
+    description: {
+        width: "90%",
+        color: "#750180",
+        fontSize: 16,
+        marginBottom: 12,
+        textAlign: "center",
+        backgroundColor: "#FC9483",
+    },
+
+    bio:{
+        color: "#750180",
+        backgroundColor: "#FC9483", 
+        fontSize: 16,
+        marginBottom: 15,
+        marginLeft: 15,
+        justifyContent: "flex-start",
+        alignSelf: "flex-start",
     },
 
     picture: {
         width: 200,
         height: 200,
         borderRadius: 75,
-        marginBottom: 20
+        marginBottom: 20,
+        backgroundColor: "#ffffff",
     },
 
     segment:{
+        backgroundColor: "#FC94AF",
+        width: "90%",
         fontSize: 24,
         marginBottom: 12,
-        justifyContent: "flex-start",
-        alignSelf: "flex-start",
+        display: "flex",
+        alignItems: "center",
+        textAlign: "center",
     },
 
     topics:{
+        backgroundColor: "#FC9483", 
+        marginLeft: 15,
         justifyContent: "flex-start",
         alignSelf: "flex-start",
     },
